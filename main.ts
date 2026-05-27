a06b03c78008f32747ab022476ee59dff5172677
Sivuyile Mtwetwe <sivuyilemtwetwe@gmail.com>
2026-05-27 15:03:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    for (let rps = 0; rps <= randint(0, 3); rps++) {
+        if (rps == 0) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else if (rps == 1) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+        } else {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+    }
 })
