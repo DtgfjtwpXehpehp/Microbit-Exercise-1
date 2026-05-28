f8e7845cb1f652fc3512ac465f501ea1d2b997b4
Sivuyile Mtwetwe <sivuyilemtwetwe@gmail.com>
2026-05-28 11:56:52 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,69 @@
+input.onButtonPressed(Button.A, function () {
+    playerAwins += 1
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    playerTied += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    playerBwins += 1
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
+    basic.clearScreen()
+})
 input.onGesture(Gesture.Shake, function () {
-    for (let rps = 0; rps <= randint(0, 3); rps++) {
-        if (rps == 0) {
-            basic.showLeds(`
-                . . . . .
-                . # # # .
-                . # # # .
-                . # # # .
-                . . . . .
-                `)
-        } else if (rps == 1) {
-            basic.showLeds(`
-                # # # # #
-                # . . . #
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-        } else {
-            basic.showLeds(`
-                # # . . #
-                # # . # .
-                . . # . .
-                # # . # .
-                # # . . #
-                `)
-        }
-    }
+    basic.showString("Total rounds played: ")
+    basic.showNumber(playerAwins + (playerBwins + playerTied))
+    basic.showString("Wins!")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.showNumber(playerAwins)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
+    basic.showNumber(playerBwins)
+    basic.pause(1000)
+    basic.showString("Tied")
+    basic.showNumber(playerTied)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+let playerTied = 0
+let playerBwins = 0
+let playerAwins = 0
+playerAwins = 0
+playerBwins = 0
+playerTied = 0
+basic.forever(function () {
+	
 })
